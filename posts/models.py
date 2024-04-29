ce4049b2c999dfc3d1e4f9201af12edf74a4757b
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-04-30 00:00:31 +0200

    unique titles

--- posts/models.py
+++ posts/models.py
@@ -7,7 +7,7 @@ from django.db import models
 
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, unique=True)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
