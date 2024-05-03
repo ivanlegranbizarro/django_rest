e507e1b0e69d8b11f5fa86209522764e2db2bb9c
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-05-03 09:50:05 +0200

    create custom user model

--- simpleblog/settings.py
+++ simpleblog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "posts",
+    "accounts",
     # Third party
     "rest_framework",
 ]
@@ -124,3 +125,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.User"
