48b2144996dece26a3b8ff103063dca2ebe3962f
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-05-03 08:56:43 +0200

    routers

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, status
+from rest_framework import status, viewsets
 from rest_framework.response import Response
 
 from posts.models import Post
--- simpleblog/urls.py
+++ simpleblog/urls.py
@@ -17,8 +17,15 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from posts.views import PostViewset
+
+router = DefaultRouter()
+
+router.register("", PostViewset, basename="posts")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("posts/", include("posts.urls", namespace="posts")),
+    path("posts/", include(router.urls)),
 ]
