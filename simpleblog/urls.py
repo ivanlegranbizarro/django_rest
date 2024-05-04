44ad8f27b5641e988c14a010566fa64162251e46
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-05-04 19:01:11 +0200

    simple_jwt setup

--- simpleblog/urls.py
+++ simpleblog/urls.py
@@ -17,9 +17,16 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("posts/", include("posts.urls", namespace="posts")),
     path("auth/", include("accounts.urls", namespace="auth")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
